fef836d3304f76b6697b0ffe5ec126272da5b260
PavelStalnenko <64463497+PavelStalnenko@users.noreply.github.com>
2021-06-20 15:59:32 +0300

    ДЗ пункты 1-6

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Pavel Stalnenko on 20.06.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBAction func modalAlert(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
